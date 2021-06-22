f5e765b2c7223faf9c7891a6daf800e27b9371d9
fabio <fabio.biliatto@gmail.com>
2021-06-21 22:39:44 -0300

    Configuração inicial testng 7.4.0 14/06 - Criou menu da calculadora no método Main, a chamada da função somar dois números e o seu respectivo Teste Unitário. 16/06 - Realizado finalização de teste unitário e inicio de API

--- src/main/java/devcalc/Calc.java
+++ src/main/java/devcalc/Calc.java
@@ -12,26 +12,27 @@ public class Calc {
     public static void main(String[] args){
 
         // Exibe o menu da calculadora
-        System.out.println(">>> CALCULADORA <<<"); // retirar o ln do print ele não pula uma linha
+        System.out.println(">>> CALCULADORA <<<");
         System.out.println("(1) Somar");
         System.out.println("(2) Subtrair");
         System.out.println("(3) Multiplicar");
         System.out.println("(4) Dividir");
-        System.out.print("Escolha o Calculo Desejado");
+        System.out.print("Escolha o Calculo Desejado: ");
 
         // Recebe a opção desejada
         int opcao = entrada.nextInt();
-        int num1 =0;
-        int num2 =0;
-
+        int num1 = 0;
+        int num2 = 0;
         // Pergunta os valores a serem usados no calculo
-           if(opcao >= 1 && opcao <=4) {
+
+        if (opcao >= 1 && opcao <= 4) {
             System.out.print("Entre o 1º numero: ");
             num1 = entrada.nextInt();
             System.out.print("Entre o 2º numero: ");
             num2 = entrada.nextInt();
             System.out.print("O resultado é: ");
         }
+
         // Chama a função do calculo desejado
         switch (opcao){
             case 1:
@@ -43,10 +44,24 @@ public class Calc {
                 break;
         }
 
-
     }
 
     public static int somarDoisNumeros(int num1, int num2){
         return num1 + num2;
+    }
+
+    public static int subtrairDoisNumeros(int num1, int num2){
+        // Todo: programar a subtração
+        return 0;
+    }
+
+    public static int multiplicarDoisNumeros(int num1, int num2){
+        // Todo: programar a multiplicação
+        return 0;
+    }
+
+    public static int dividirDoisNumeros(int num1, int num2){
+        // Todo: programar a divisão
+        return 0;
     }
 }
--- src/test/java/unitTests/TestaCalc.java
+++ src/test/java/unitTests/TestaCalc.java
@@ -3,30 +3,44 @@ package unitTests;
 import devcalc.Calc;
 import org.testng.annotations.Test;
 
-
 import static org.testng.Assert.assertEquals;
 
-public class TestaCalc {
 
-    @Test
-    public void testarSomarDoisNumeros () {
-        // 1° Parte do teste é o PREPARAR - CONFIGURAR -GIVEN
+public class TestaCalc {
+    @Test (priority = 1)
+    public void testarSomarDoisNumeros() {
+        // 1 - Prepara - Configurar - Given - Arrange
         int num1 = 5;
         int num2 = 7;
         int resultadoEsperado = 12;
 
-        //2° EXECUTAR - WHEN
+        // 2 - Executar - When - Act
         int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
 
-        //3° VALIDAR -THEN
+        // 3 - Validar - Then - Assert
         System.out.println("O resultado Esperado é " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
 
-        assertEquals(resultadoEsperado, resultadoAtual);
+        assertEquals(resultadoAtual, resultadoEsperado);
 
     }
 
-    @Test
-    public void testarMain(){
+    @Test (priority = 2)
+    public void testarSubtrairDoisNumeros(){
+        int num1 = 6;
+        int num2 = 2;
+        int resultadoEsperado = 4;
+
+        int resultadoAtual = Calc.subtrairDoisNumeros(num1,num2);
+
+        System.out.println("O resultado esperado é " + resultadoEsperado
+                + " e o resultado atual é " + resultadoAtual);
+
+        assertEquals(resultadoAtual,resultadoEsperado);
 
     }
+
+
+
+
 }
+
