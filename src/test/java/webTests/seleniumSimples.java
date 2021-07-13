9701f781d3babfa7e5f50bf156022b4a4cd3cec0
fabio <fabio.biliatto@gmail.com>
2021-07-12 22:43:42 -0300

    Configuração inicial testng 7.4.0 14/06 - Criou menu da calculadora no método Main, a chamada da função somar dois números e o seu respectivo Teste Unitário. 16/06 - Realizado finalização de teste unitário e inicio de API

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -7,21 +7,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import utils.Evidencias;
 
 // 3 - Classe
 public class seleniumSimples {
     // 3.1 - Atributos
 
     WebDriver driver;                           // declarar o objeto do Selenium WebDriver
+    Evidencias evidencias;
+    static String dataHora = new SimpleDateFormat("yyyy-MM-dd HH-mm").getCalendar().getTime().toString();
 
     // 3.2 - Métodos e Funções
     @BeforeMethod
@@ -32,6 +35,8 @@ public class seleniumSimples {
         // Instancia o objeto driver como um controlador do Chrome
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+
+        evidencias = new Evidencias(); // instanciar
     }
 
     @AfterMethod
@@ -43,48 +48,55 @@ public class seleniumSimples {
     }
 
     @Test(priority = 1)
-    public void consultarCursoMantis(){
-
+    public void consultarCursoMantis() throws IOException {
+        String casoDeTeste = "Consultar Curso Mantis";
         // B - Realizar o teste
         driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        evidencias.print(driver, dataHora, casoDeTeste, "Passo 1 - Abriu o site");
 
         driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
         driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
         driver.findElement(By.id("searchtext")).sendKeys("mantis");  // Escreve "mantis" no campo
 
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou Mantis");
         driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
 
         assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
         //assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
         //assertEquals(driver.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "Cursos › \"mantis\"");
 
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
         driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
 
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
-
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
     }
 
     @Test(priority = 2, dependsOnMethods = {"consultarCursoMantis"})
-    public void consultarCursoCTFL(){
+    public void consultarCursoCTFL() throws IOException {
+        String casoDeTeste = "Consultar Curso CTFL";
 
         // B - Realizar o teste
         driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 1 - Abriu o site");
 
         driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
         driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
         driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");  // Escreve "mantis" no campo
 
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou preparatorio ctfl");
         driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
 
         assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"preparatório ctfl\"");
         //assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
         //assertEquals(driver.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "Cursos › \"mantis\"");
 
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
         driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
 
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText().toUpperCase(), "PREPARATÓRIO CTFL");
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00");
-
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
     }
 }
